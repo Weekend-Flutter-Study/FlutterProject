6126353e836c1bc7fabba03fe0078ebdf3be23c4
kyuseong <ksjmgrkks@naver.com>
2023-07-02 19:37:58 +0900

    [feature] Future 공부 및 WebView 마무리 작업

--- kks/chapter0to7/lib/chapter03/future.dart
+++ kks/chapter0to7/lib/chapter03/future.dart
@@ -1,3 +1,27 @@
-class Future {
-  void run() {}
+Future<void> main() async {
+   await addNumbers2(1, 2);
+   addNumbers2(2, 3);
+}
+
+void addNumbers1(int number1, int number2) {
+  print('계산 시작!');
+  var sum = 0;
+
+  Future.delayed(const Duration(seconds: 3), () {
+    sum = number1 + number2;
+  });
+
+  print('합 : $sum');
+}
+
+Future<void> addNumbers2(int number1, int number2) async {
+  print('계산 시작!');
+  var sum = 0;
+
+  await Future.delayed(const Duration(seconds: 3), () {
+    print('더하는중...');
+    sum = number1 + number2;
+  });
+
+  print('합 : $sum');
 }
