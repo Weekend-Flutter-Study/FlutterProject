f488821c915946f5d6f21084839664c50fd02c2e
kyuseong <ksjmgrkks@naver.com>
2023-08-19 19:01:45 +0900

    [kks] section21 프로젝트 완료

---
+++ kks/section21/lib/model/schedule_with_color.dart
@@ -0,0 +1,11 @@
+import 'package:section21/database/drift_database.dart';
+
+class ScheduleWithColor {
+  final Schedule schedule;
+  final CategoryColor categoryColor;
+
+  ScheduleWithColor({
+    required this.schedule,
+    required this.categoryColor,
+  });
+}
