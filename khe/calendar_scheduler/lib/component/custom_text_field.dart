98f0a24620ccc2defac87e7aa60c653eafcc10a3
Hyoeun Kwon <kkunghe.d@gmail.com>
2023-08-21 23:33:30 +0900

    Chapter21. calendar_scheduler : textfield

--- khe/calendar_scheduler/lib/component/custom_text_field.dart
+++ khe/calendar_scheduler/lib/component/custom_text_field.dart
@@ -1,10 +1,13 @@
 import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
+  final bool isTime;
   const CustomTextField({
     required this.label,
+    required this.isTime,
     Key? key}) : super(key: key);
 
   @override
@@ -15,15 +18,25 @@ class CustomTextField extends StatelessWidget {
         Text(label,
           style: TextStyle(color: PRIMARY_COLOR, fontWeight: FontWeight.w600),
         ),
-        TextField(
-          cursorColor: Colors.grey,
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            filled: true,
-            fillColor: Colors.grey[300],
-          ),
-        ),
+        if(isTime) renderTextField(),
+        if(!isTime)
+          Expanded(child: renderTextField())
       ],
+    );
+  }
+  Widget renderTextField(){
+    return TextField(
+      cursorColor: Colors.grey,
+      expands: !isTime,
+      keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
+      inputFormatters: isTime ? [
+        FilteringTextInputFormatter.digitsOnly
+      ] : [],
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Colors.grey[300],
+      ),
     );
   }
 }
