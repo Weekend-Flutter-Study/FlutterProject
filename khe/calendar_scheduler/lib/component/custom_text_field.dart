4765368a3f1363c7991ddda6b986b2eb0f1bd578
Hyoeun Kwon <kkunghe.d@gmail.com>
2023-08-27 12:32:39 +0900

    [khe] Chapter21. calendar_scheduler : use code generation and textformfield

--- khe/calendar_scheduler/lib/component/custom_text_field.dart
+++ khe/calendar_scheduler/lib/component/custom_text_field.dart
@@ -4,10 +4,13 @@ import 'package:flutter/services.dart';
 
 class CustomTextField extends StatelessWidget {
   final String label;
+  // true - 시간 / false - 내용
   final bool isTime;
+  final FormFieldSetter<String> onSaved;
   const CustomTextField({
     required this.label,
     required this.isTime,
+    required this.onSaved,
     Key? key}) : super(key: key);
 
   @override
@@ -25,8 +28,39 @@ class CustomTextField extends StatelessWidget {
     );
   }
   Widget renderTextField(){
-    return TextField(
+    return TextFormField(
+      // 외부에서 입력
+      onSaved: onSaved,
+      // FORM을 가지고 동시에 관리
+      // onChange를 쓸 수 있지만 안써도 o
+      validator: (String? val){
+        // validator : null 이 return 되면 에러가 없다.
+        // 에러가 있으면 에러를 String 값으로 리턴해준다.
+       if(val == null || val.isEmpty){
+         return '값을 입력해주세요';
+       }
+
+       if(isTime){
+         int time = int.parse(val);
+         if(time < 0){
+           return '0 이상의 숫자를 입력해주세요';
+         }
+
+         if(time > 24){
+           return '24 이하의 숫자를 입력해주세요';
+         }
+       }else{
+         if(val.length > 500){
+           return '500자 이하의 글자를 입력해주세요.';
+         }
+       }
+
+       return null;
+        //기본은 return null;
+      },
       cursorColor: Colors.grey,
+      maxLines: isTime ? 1 : null,
+      // maxLength: 500,
       expands: !isTime,
       keyboardType: isTime ? TextInputType.number : TextInputType.multiline,
       inputFormatters: isTime ? [
--- khe/calendar_scheduler/lib/component/schedule_bottom_sheet.dart
+++ khe/calendar_scheduler/lib/component/schedule_bottom_sheet.dart
@@ -3,23 +3,41 @@ import 'package:flutter/material.dart';
 
 import '../const/colors.dart';
 
-class ScheduleBottomSheet extends StatelessWidget {
+class ScheduleBottomSheet extends StatefulWidget {
+  // text의 상태 관리를 위해 stateless -> stateful로 변경
   const ScheduleBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  State<ScheduleBottomSheet> createState() => _ScheduleBottomSheetState();
+}
+
+class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  int? startTime;
+  int? endTime;
+  String? content;
 
   @override
   Widget build(BuildContext context) {
     //viewInsets : 시스템으로 가려진 부분 가져 올 수 있음
-    final bottomInset = MediaQuery.of(context).viewInsets.bottom; // 키보드가 차지하는 부분이 몇 픽셀인지 알 수 있음
+    final bottomInset = MediaQuery
+        .of(context)
+        .viewInsets
+        .bottom; // 키보드가 차지하는 부분이 몇 픽셀인지 알 수 있음
 
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         // 키보드 닫힘
         FocusScope.of(context).requestFocus(FocusNode());
       },
       child: SafeArea(
         child: Container(
           color: Colors.white,
-          height: MediaQuery.of(context).size.height / 2 + bottomInset,
+          height: MediaQuery
+              .of(context)
+              .size
+              .height / 2 + bottomInset,
           child: Padding(
             padding: EdgeInsets.only(bottom: bottomInset),
             child: Padding(
@@ -28,17 +46,40 @@ class ScheduleBottomSheet extends StatelessWidget {
                 right: 8.0,
                 top: 16.0,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _Time(),
-                  SizedBox(height: 16.0),
-                  _Content(),
-                  SizedBox(height: 16.0),
-                  _ColorPicker(),
-                  SizedBox(height: 8.0),
-                  _SaveButton(),
-                ],
+              // TextFormField를 이용해 한번에 관리하기 위해 여러 텍스트필드 상단 위에 Form을 래핑하여 사용,
+              // Column 위에도 괜찮고, 더 상단도 괜찮음
+              child: Form(
+                // key : Form의 일종의 컨트롤러
+                key: formKey,
+                autovalidateMode: AutovalidateMode.always,
+                // 저장 키를 누르지 않아도, 입력할때마다 검증가능
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _Time(
+                      onStartSaved: (String? val){
+                        // 값을 저장하여 어딘가에서 반복적 사용하길 원함
+                        startTime = int.parse(val!);
+                      },
+                      onEndSaved: (String? val){
+                        // validate 에서 null 이면 에러 메세지 던지라고 했기 때문에 ! 해도 괜찮다.
+                        endTime = int.parse(val!);
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    _Content(
+                      onSaved: (String? val){
+                        content = val;
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    _ColorPicker(),
+                    SizedBox(height: 8.0),
+                    _SaveButton(
+                      onPressed: onSavePressed,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -46,10 +87,38 @@ class ScheduleBottomSheet extends StatelessWidget {
       ),
     );
   }
+
+  void onSavePressed() {
+    // formKey는 생성을 했는데, Form 위젯과 결합을 안했을 때
+    if (formKey.currentState == null) {
+      return;
+    }
+
+    // formkey 사용하여 3개의 텍스트필드를 검증 - 한번에 다 실행.
+    if (formKey.currentState!.validate()) {
+      // String을 return 하면 에러메세지로 간주하기 때문에 우리를 이를 활용하여 에러메세지로 보여줄 수 있음
+      // null을 리턴하면 에러가 없다. 모든 텍스트 필드가 null 을 리턴하면 true 뭐라도 있으면 false
+      print('에러가 없습니다.');
+      // 에러가 없으면 저장
+      formKey.currentState!.save();
+      print('------------------');
+      print('startTime : $startTime');
+      print('endTime : $endTime');
+      print('content : $content');
+    } else {
+      print('에러가 있습니다.');
+    }
+  }
 }
 
 class _Time extends StatelessWidget {
-  const _Time({Key? key}) : super(key: key);
+  final FormFieldSetter<String> onStartSaved;
+  final FormFieldSetter<String> onEndSaved;
+
+  const _Time({
+    required this.onStartSaved,
+    required this.onEndSaved,
+    Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -59,6 +128,7 @@ class _Time extends StatelessWidget {
           child: CustomTextField(
             label: '시작 시간',
             isTime: true,
+            onSaved: onStartSaved,
           ),
         ),
         SizedBox(
@@ -68,6 +138,7 @@ class _Time extends StatelessWidget {
           child: CustomTextField(
             label: '마감 시간',
             isTime: true,
+            onSaved: onEndSaved,
           ),
         ),
       ],
@@ -76,16 +147,21 @@ class _Time extends StatelessWidget {
 }
 
 class _Content extends StatelessWidget {
-  const _Content({Key? key}) : super(key: key);
+  final FormFieldSetter<String> onSaved;
+
+  const _Content({
+    required this.onSaved,
+    Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: CustomTextField(
+      child: CustomTextField(
         label: '내용',
         isTime: false,
-    ),
-      );
+        onSaved: onSaved,
+      ),
+    );
   }
 }
 
@@ -122,7 +198,11 @@ class _ColorPicker extends StatelessWidget {
 }
 
 class _SaveButton extends StatelessWidget {
-  const _SaveButton({Key? key}) : super(key: key);
+  final VoidCallback onPressed;
+
+  const _SaveButton({
+    required this.onPressed,
+    Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -130,7 +210,7 @@ class _SaveButton extends StatelessWidget {
       children: [
         Expanded(
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: onPressed,
             style: ElevatedButton.styleFrom(
               backgroundColor: PRIMARY_COLOR,
             ),
