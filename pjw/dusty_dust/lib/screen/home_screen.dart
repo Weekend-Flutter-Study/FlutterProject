8b8ad7aa097da080bb09f12b743518d17f56a319
Hyoeun Kwon <112844182+LaimaPark@users.noreply.github.com>
2023-09-06 21:48:01 +0900

    [pjw] Chapter23. dusty_dust

---
+++ pjw/dusty_dust/lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        backgroundColor: primaryColor,
+        body: CustomScrollView(
+          slivers: [
+            MainAppBar(),
+          ],
+        ),
+    );
+  }
+}
